b16b333a950abb965f0064d5e51e9805dbe00dc8
Yevhen Kondratyev <ykondrat@e1r1p9.unit.ua>
2017-03-17 17:49:45 +0200

    get next line_NORME

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykondrat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/22 11:20:37 by ykondrat          #+#    #+#             */
+/*   Updated: 2017/03/17 17:36:04 by ykondrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 16
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
